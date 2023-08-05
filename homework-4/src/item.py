b332af9ecc7676aef6584376341b4a85e7fb02e8
Klasis <d23kv321@gmail.com>
2023-08-05 16:34:11 +0300

    commit

--- homework-4/src/item.py
+++ homework-4/src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,7 +16,31 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return f"{self.__name}"
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_):
+        self.__name = name_
+        if len(self.__name) > 10:
+            self.__name = self.__name[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +48,22 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.quantity * self.price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        Item.all = []
+        with open(path, 'r', encoding="utf-8") as csvfile:
+            reader: csv.DictReader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(str_):
+        return int(float(str_))
